28eb1cc81b3f66b0c4300e629c8bfecb3bec3532
kjaunslavietis <kjaunslavietis@gmail.com>
2020-04-25 02:13:33 +0200

    Added initial UI structure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import { API, graphqlOperation } from 'aws-amplify'
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import { getOrCreateMeeting, getDemoResponse } from './graphql/queries'
+import { Container, Row, Col, Jumbotron, Navbar, Button, Form } from 'react-bootstrap';
+import Browser from './components/Browser';
 
-const initialState = { desiredMeetingId: '', actualMeetingId: '' }
+import './App.scss';
 
-function App() {
-  const [formState, setFormState] = useState(initialState)
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
+const mockConvos = [
+  {
+    name: "Conversation 1",
+    description: "Conversation 1 description"
+  },
+  {
+    name: "Conversation 2",
+    description: "Conversation 2 description"
+  },
+  {
+    name: "Conversation 3",
+    description: "Conversation 3 description"
+  },
+  {
+    name: "Conversation 4",
+    description: "Conversationm 4 description"
+  },
+  {
+    name: "Conversation 5",
+    description: "Conversation 5 description"
+  },
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      conversations: mockConvos,
+      selectedConversation: null
+    };
+
+    this.conversationInfo = this.conversationInfo.bind(this);
   }
 
-  async function joinMeeting() {
-    try {
-      let meetingAndAttendeeInfo = await API.graphql(graphqlOperation(getOrCreateMeeting, {meetingId: formState.desiredMeetingId}));
-      console.log(JSON.stringify(meetingAndAttendeeInfo));
-    } catch(err) {
-      console.error(JSON.stringify(err));
+  noConversationSelected() {
+    return (
+      <Container>
+        <h1>Select conversation</h1>
+        <p>
+          You are not currently in a conversation. Select a conversation from the left to join.
+        </p>
+      </Container>
+    )
+  }
+
+  conversationInfo() {
+    if(this.state.selectedConversation) {
+      return (
+        <Container>
+          <h1>Conversation info</h1>
+          <h3><strong>Conversation name:</strong> {this.state.selectedConversation.name}</h3>
+          <p>
+            <strong>Conversation description:</strong> {this.state.selectedConversation.description}
+          </p>
+        </Container>
+      )
+    } else {
+      return this.noConversationSelected();
     }
   }
 
-  return (
-    <div style={styles.container}>
-      <h2>Join a meeting</h2>
-      <input
-        onChange={event => setInput('desiredMeetingId', event.target.value)}
-        style={styles.input}
-        value={formState.desiredMeetingId} 
-        placeholder="Meeting ID"
-      />
-      <button style={styles.button} onClick={joinMeeting}>Join meeting</button>
-    </div>
-  );
-}
-
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+  render() {
+    return (
+      <Container fluid>
+        <Navbar className="bg-light justify-content-between">
+          <Navbar.Brand>Chime Discover</Navbar.Brand>
+          <Form inline>
+            <Button variant="primary">Create Conversation</Button>
+          </Form>
+        </Navbar>
+        <Row id="mainRow">
+          <Col id="searchCol" sm={2}>
+            <Browser 
+              conversations={this.state.conversations}
+              onConversationSelected={(id) => this.setState({selectedConversation: this.state.conversations[id]})}
+              />
+          </Col>
+          <Col id="mainCol" sm={10}>
+            <Jumbotron fluid>
+                {this.conversationInfo()}
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
