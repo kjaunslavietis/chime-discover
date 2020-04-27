66616e5733b19374f2cfea6e98bb9b5d5b14994c
Paula <pssalib@amazon.lu>
2020-04-27 09:37:49 +0200

    chat api

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,57 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createChatMessage = /* GraphQL */ `
+  mutation CreateChatMessage(
+    $input: CreateChatMessageInput!
+    $condition: ModelChatMessageConditionInput
+  ) {
+    createChatMessage(input: $input, condition: $condition) {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateChatMessage = /* GraphQL */ `
+  mutation UpdateChatMessage(
+    $input: UpdateChatMessageInput!
+    $condition: ModelChatMessageConditionInput
+  ) {
+    updateChatMessage(input: $input, condition: $condition) {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteChatMessage = /* GraphQL */ `
+  mutation DeleteChatMessage(
+    $input: DeleteChatMessageInput!
+    $condition: ModelChatMessageConditionInput
+  ) {
+    deleteChatMessage(input: $input, condition: $condition) {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const createRoom = /* GraphQL */ `
   mutation CreateRoom(
     $input: CreateRoomInput!
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,48 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const onCreateChatMessage = /* GraphQL */ `
+  subscription OnCreateChatMessage {
+    onCreateChatMessage {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateChatMessage = /* GraphQL */ `
+  subscription OnUpdateChatMessage {
+    onUpdateChatMessage {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteChatMessage = /* GraphQL */ `
+  subscription OnDeleteChatMessage {
+    onDeleteChatMessage {
+      id
+      roomID
+      content
+      senderName
+      mediaUrl
+      mediaThumbnail
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const onCreateRoom = /* GraphQL */ `
   subscription OnCreateRoom {
     onCreateRoom {
