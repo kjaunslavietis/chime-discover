1cf83187a6862a21db9be901e0afb3f89b4fab75
Kate Abileva <k.abileva@gmail.com>
2020-05-13 03:24:02 +0200

    audio test

--- src/components/ActiveConversation.js
+++ src/components/ActiveConversation.js
@@ -195,7 +195,7 @@ class ActiveConversation extends React.Component {
             const audioInputDeviceInfo = devices.input;
             const inputDeviceId = audioInputDeviceInfo[0].deviceId;
             console.log('Input audio device: ', audioInputDeviceInfo[0]);
-            // await this.meetingSession.audioVideo.chooseAudioInputDevice(inputDeviceId);
+            await this.meetingSession.audioVideo.chooseAudioInputDevice(inputDeviceId);
             // const audioOutputDeviceInfo = devices.output;
             // const outputDeviceId = audioOutputDeviceInfo[0].deviceId;
             // console.log('Ouput audio device: ', audioOutputDeviceInfo[0]);
